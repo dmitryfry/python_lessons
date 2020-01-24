a17c5e9e427cb63fb941bae6b398d7ee2f7215ba
Dmitry Fry <im@dmitryfry.com>
2020-01-24 13:35:21 +0300

    solutions

---
+++ issue_2.py
@@ -0,0 +1,10 @@
+import time
+
+seconds = int(input('введите пожалуйста секунды: '))
+
+print(time.strftime("%H:%M:%S", time.gmtime(seconds)))
+
+
+
+
+
---
+++ issue_4.py
@@ -0,0 +1,6 @@
+n = list(map(int, input('введите целое положительное число: ')))
+
+while 1:
+    print(max(n))
+    break
+
